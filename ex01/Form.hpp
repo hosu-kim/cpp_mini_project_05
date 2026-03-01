05673d17a37197e339c7c2cf81c94e359f412e5e
hosu-kim <hosu@outlook.cz>
2026-03-01 21:59:44 +0100

    commit

---
+++ ex01/Form.hpp
@@ -0,0 +1,13 @@
+#ifndef FORM_HPP
+# define FORM_HPP
+
+#include <string>
+
+class Form {
+	private:
+		std::string _name;
+		bool _isSigned;
+		
+}
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,33 @@
+#include "Bureaucrat.hpp"
+
+int main() {
+	try {
+		Bureaucrat b1("Top Officer", 1);
+		std::cout << b1 << std::endl;
+
+		b1.decrementGrade();
+		std::cout << "After decrement: " << b1 << std::endl;
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << "-------------------" << std::endl;
+
+	try {
+		Bureaucrat b2("The God", 0);
+	} catch (std::exception &e) {
+		std::cerr << "Cause expected exception: " << e.what() << std::endl;
+	}
+
+	std::cout << "-------------------" << std::endl;
+
+	try {
+		Bureaucrat b3("Worker", 150);
+		std::cout << b3 << std::endl;
+		b3.decrementGrade();
+	} catch (std::exception &e) {
+		std::cerr << "Caught expected exception: " << e.what() << std::endl;
+	}
+
+	return 0;
+}
