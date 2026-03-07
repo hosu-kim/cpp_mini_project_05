6cab4e3e2c0a771cb6c3679df333cf5fdbe3423e
hoskim <hosu@outlook.cz>
2026-03-07 14:35:44 +0100

    commit

--- ex02/AForm.hpp
+++ ex02/AForm.hpp
@@ -31,6 +31,8 @@ class AForm {
 
 		void			beSigned(const Bureaucrat& b);
 
+		// 추상 클래스를 만들 때 멤버 함수를 0으로 초기화하는 이유는
+		// 이 함수는 몸체가 없으므로 자식이 채워야 한다는 것을 표현하는 것이다.
 		virtual void	execute(Bureaucrat const & executor) const = 0;
 
 		void			checkExecutable(Bureaucrat const & executor) const;
